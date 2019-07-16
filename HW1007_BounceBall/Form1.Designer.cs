a0f65c9eb9b9a6bc5948a6762b93e7fc06330a22
ErezHackeru <45459156+ErezHackeru@users.noreply.github.com>
2019-07-16 16:28:09 +0300

    Add files via upload

---
+++ HW1007_BounceBall/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace HW1007_BounceBall
+{
+    partial class BB_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BallPicBx = new System.Windows.Forms.PictureBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.AddBallBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.BallPicBx)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BallPicBx
+            // 
+            this.BallPicBx.Image = global::HW1007_BounceBall.Properties.Resources.Ball_Pic;
+            this.BallPicBx.Location = new System.Drawing.Point(700, 350);
+            this.BallPicBx.Name = "BallPicBx";
+            this.BallPicBx.Size = new System.Drawing.Size(100, 100);
+            this.BallPicBx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BallPicBx.TabIndex = 0;
+            this.BallPicBx.TabStop = false;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(415, 418);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 23);
+            this.StartBtn.TabIndex = 1;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // AddBallBtn
+            // 
+            this.AddBallBtn.Location = new System.Drawing.Point(332, 418);
+            this.AddBallBtn.Name = "AddBallBtn";
+            this.AddBallBtn.Size = new System.Drawing.Size(75, 23);
+            this.AddBallBtn.TabIndex = 2;
+            this.AddBallBtn.Text = "Add a Ball";
+            this.AddBallBtn.UseVisualStyleBackColor = true;
+            this.AddBallBtn.Click += new System.EventHandler(this.AddBallBtn_Click);
+            // 
+            // BB_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.AddBallBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.BallPicBx);
+            this.Name = "BB_Form";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.BallPicBx)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox BallPicBx;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button AddBallBtn;
+    }
+}
+
---
+++ HW1007_BounceBall/TimerExample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1007_BounceBall
+{
+    class TimerExample
+    {
+        private void allRelevantCode()
+        {
+            System.Timers.Timer timer = new System.Timers.Timer(10);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Enabled = true;
+
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+        //    Action a = () =>
+        //    {
+        //        NewX = NewX + dx;
+        //        NewY = NewY + dy;
+        //        BallPicBx.Location = new Point(NewX, NewY);
+
+        //        if (NewX == X_WINDOW_SIZE + 1)
+        //        {
+        //            dx = -1;
+        //        }
+        //        if (NewY == Y_WINDOW_SIZE + 1)
+        //        {
+        //            dy = -1;
+        //        }
+        //        if (NewX == 100)
+        //        {
+        //            dx = 1;
+        //        }
+        //        if (NewY == 100)
+        //        {
+        //            dy = 1;
+        //        }
+        //    };
+
+        //    this.BeginInvoke(a);
+        }
+    }
+}
